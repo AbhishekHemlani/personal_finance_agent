6f15c297d19809846915e62075e083fc09d973e7
AbhishekHemlani <abhishekhemlani@outlook.com>
2026-05-17 17:17:17 -0400

    Disable service worker during local development

--- src/main.jsx
+++ src/main.jsx
@@ -9,8 +9,12 @@ createRoot(document.getElementById("root")).render(
   </React.StrictMode>,
 );
 
-if ("serviceWorker" in navigator) {
+if ("serviceWorker" in navigator && import.meta.env.PROD) {
   window.addEventListener("load", () => {
     navigator.serviceWorker.register("/sw.js");
+  });
+} else if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    registrations.forEach((registration) => registration.unregister());
   });
 }
